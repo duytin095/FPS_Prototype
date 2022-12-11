f5da6b5f38299ccd0af8fd8a6d555d942ad69748
duytin095 <duytin095@gmail.com>
2022-12-11 23:38:17 +0700

    starter assets

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -5,18 +5,30 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private CharacterController characterController;
+    [SerializeField] private Animator animator;
     [SerializeField] private float PosX;
     [SerializeField] private float PosZ;
     [SerializeField] private float speed;
+    [SerializeField] private string movingState = "isMove";
     void Start()
     {
         characterController = transform.GetComponent<CharacterController>();
+        animator = transform.GetComponent<Animator>();
     }
 
     void Update()
     {
         PosX = Input.GetAxis("Horizontal");
         PosZ = Input.GetAxis("Vertical");
+
+        if(PosX != 0 || PosZ != 0)
+        {
+            animator.SetBool(movingState, true);
+        }
+        else
+        {
+            animator.SetBool(movingState, false);
+        }
     }
 
     private void FixedUpdate()
