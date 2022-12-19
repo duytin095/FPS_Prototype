69aa39873978499df94d8f3ebb5e4bac5fe63201
duytin095 <duytin095@gmail.com>
2022-12-19 18:33:06 +0700

    import assets

--- Assets/Script/WeaponController.cs
+++ Assets/Script/WeaponController.cs
@@ -5,12 +5,15 @@ public class WeaponController : MonoBehaviour
 {
     [SerializeField] private Animator _anim;
     [SerializeField] private bool isShoot = true;
-    [SerializeField] private ParticleSystem muzzleEffect;
-    [SerializeField] private Transform gunMouth;
+    
     [SerializeField] private Camera cam;
     [SerializeField] private float maxDistance;
-    
-    
+
+    [Header("VFX")]
+    [SerializeField] private ParticleSystem muzzleEffect; 
+    [SerializeField] private GameObject hitEffect;
+
+
     public void Shoot()
     {
         if (isShoot && AmmoHandle.Instance.ammoValue > 0)
@@ -26,7 +29,6 @@ public class WeaponController : MonoBehaviour
             Ray hitRay = new Ray(cam.transform.position, cam.transform.forward);
             if (Physics.Raycast(hitRay, out hit, maxDistance))
             {
-                Debug.Log(hit.transform.name);
                 if (hit.transform.CompareTag("Enemy"))
                 {
                     Enemy.Instance.LostHealth(3);
