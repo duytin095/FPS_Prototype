43edff596952e677295c005b1923c1eb24cdf2f0
duytin095 <duytin095@gmail.com>
2022-12-20 16:29:47 +0700

    simple enemy shooting system

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -1,19 +1,24 @@
 using UnityEngine;
 using DG.Tweening;
+using System.Collections;
 
 public class Enemy : MonoBehaviour
 {
     public static Enemy Instance;
 
-    [SerializeField] private float enemyHealth; 
-    [SerializeField] private float maxDistance; 
+    [SerializeField] private float enemyHealth; // Enemy health, if this number <= 0 so enemy "Death"
+    [SerializeField] private float maxDistance; // Max distance that enemy is able to see player
+    [SerializeField] private float gunMaxDistance; // Max distance that enemy is able to see player
     [SerializeField] private Animator animator;
-    [SerializeField] private GameObject burstSign;
-    [SerializeField] private GameObject player;
-    [SerializeField] private Transform lookAtPos;
+    [SerializeField] private GameObject burstSign; // Burst sign when enemy see player
+    [SerializeField] private GameObject player; 
+    [SerializeField] private Transform lookAtPos; // Looking position on player
+    [SerializeField] private Transform aimingPos; // Aiming position on gun
 
 
     [SerializeField] private bool isDead = false;
+    [SerializeField] private float coolDown;
+    [SerializeField] private float coolDownLap = 4;
 
 
 
@@ -24,8 +29,11 @@ public class Enemy : MonoBehaviour
     {
         Instance = this;
     }
+
     void Update()
     {
+        coolDown -= Time.deltaTime; // Cool down time allow enemy to shooting
+
         if (enemyHealth <= 0)
         {
             EnemyState("Dying", 0);
@@ -33,7 +41,7 @@ public class Enemy : MonoBehaviour
             Destroy(this.gameObject, 7);
         }
 
-
+        
         if (player)
         {
             var dist = Vector3.Distance(player.transform.position, transform.position);
@@ -45,9 +53,18 @@ public class Enemy : MonoBehaviour
             else
             {
                 EnemyState("Aiming", 1);
-                if (!isDead)
+
+                if (!isDead && coolDown <=0)
                 {
-                    transform.LookAt(lookAtPos);
+                    transform.LookAt(lookAtPos); // Make gameobject look at target (player)
+
+                    //** Lock gameobject's rotation on x and z axis
+                    Vector3 rot = Quaternion.LookRotation(lookAtPos.position - transform.position).eulerAngles;
+                    rot.x = rot.z = 0;
+                    transform.rotation = Quaternion.Euler(rot);
+                    //**
+
+                    ShootPlayer();
                 }
             }
         }
@@ -70,4 +87,20 @@ public class Enemy : MonoBehaviour
             animator.CrossFade(animationState, 0.1f, 0);
         }
     }
+
+    private void ShootPlayer()
+    {
+        RaycastHit raycastHit;
+        Ray rayHit = new Ray(aimingPos.position, aimingPos.forward);
+        if (Physics.Raycast(rayHit, out raycastHit, gunMaxDistance))
+        {
+            if (raycastHit.transform.CompareTag("Player"))
+            {
+                Debug.Log("Bang");
+                coolDown = coolDownLap;
+            }
+        }   
+    }
+
+
 }
